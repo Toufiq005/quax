46593a7d82c17bfc77e5b771851669e32a6af243
Toufiq005 <125236484+Toufiq005@users.noreply.github.com>
2023-06-23 08:37:30 +0600

    api 4

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -3,8 +3,10 @@ import Navbar from "@/components/Navbar/Navbar";
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar bgColor="bg-white border-b" textColor="text-black/80" />
-      <main>{children}</main>
+      <main>
+        <Navbar bgColor="bg-white border-b" textColor="text-black/80" />
+        {children}
+      </main>
     </>
   );
 }
