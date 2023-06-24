3515002c8f7a692903510296d122eeaa1f1d0cce
Toufiq005 <125236484+Toufiq005@users.noreply.github.com>
2023-06-24 10:02:36 +0600

    bug fix 8

--- src/components/Sections/TeamMemberSection/TeamMemberSection.jsx
+++ src/components/Sections/TeamMemberSection/TeamMemberSection.jsx
@@ -13,7 +13,15 @@ require("dotenv").config();
 // }
 
 export default function TeamMemberSection(props) {
+  const [showLinks, setShowLinks] = useState(false);
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const res = fetch(
+      process.env.NEXT_PUBLIC_REST_API_ENDPOINT + "/team-members"
+    );
+    setData(res.json());
+  }, []);
 
   return (
     <section
@@ -31,82 +39,64 @@ export default function TeamMemberSection(props) {
             amet.
           </p>
         </div>
-        <Team limit={props.limit} />
+        <div data-aos="fade-up" className="team w-full h-auto mt-11 ">
+          {data ??
+            data.slice(0, props.limit).map((profile) => (
+              <div
+                key={profile.id}
+                className="team-member-profile overflow-hidden"
+                onMouseEnter={() => setShowLinks(true)}
+                onMouseLeave={() => setShowLinks(false)}
+              >
+                <div>
+                  <Image
+                    width={270}
+                    height={270}
+                    src={img}
+                    alt="img"
+                    className="team-member-profile-img"
+                  />
+                  <h2>{profile.name}</h2>
+                  <h3>{profile.role}</h3>
+                </div>
+                {showLinks && (
+                  <div data-aos="fade-left" className="social-links block w-10">
+                    <a href={profile.socialLinks.facebook}>
+                      <div className="w-10 h-10 rounded bg-white flex items-center justify-center ">
+                        <Image
+                          width={9}
+                          height={16}
+                          src="/assets/sections/team-members/icon-facebook.svg"
+                          alt="icon"
+                        />
+                      </div>
+                    </a>
+                    <a href={profile.socialLinks.twitter}>
+                      <div className="w-10 h-10 rounded bg-header flex items-center justify-center ">
+                        <Image
+                          width={16}
+                          height={13}
+                          src="/assets/sections/team-members/icon-twitter.svg"
+                          alt="icon"
+                        />
+                      </div>
+                    </a>
+                    <a href={profile.socialLinks.vimeo}>
+                      <div className="w-10 h-10 rounded bg-white flex items-center justify-center ">
+                        <Image
+                          width={14}
+                          height={12}
+                          src="/assets/sections/team-members/icon-vimeo.svg"
+                          alt="icon"
+                        />
+                      </div>
+                    </a>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
       </div>
     </section>
-  );
-}
-function TeamProfile({ name, img, role, socialLinks }) {
-  const [showLinks, setShowLinks] = useState(false);
-
-  return (
-    <div
-      className="team-member-profile overflow-hidden"
-      onMouseEnter={() => setShowLinks(true)}
-      onMouseLeave={() => setShowLinks(false)}
-    >
-      <div>
-        <Image
-          width={270}
-          height={270}
-          src={img}
-          alt="img"
-          className="team-member-profile-img"
-        />
-        <h2>{name}</h2>
-        <h3>{role}</h3>
-      </div>
-      {showLinks && (
-        <div data-aos="fade-left" className="social-links block w-10">
-          <a href={socialLinks.facebook}>
-            <div className="w-10 h-10 rounded bg-white flex items-center justify-center ">
-              <Image
-                width={9}
-                height={16}
-                src="/assets/sections/team-members/icon-facebook.svg"
-                alt="icon"
-              />
-            </div>
-          </a>
-          <a href={socialLinks.twitter}>
-            <div className="w-10 h-10 rounded bg-header flex items-center justify-center ">
-              <Image
-                width={16}
-                height={13}
-                src="/assets/sections/team-members/icon-twitter.svg"
-                alt="icon"
-              />
-            </div>
-          </a>
-          <a href={socialLinks.vimeo}>
-            <div className="w-10 h-10 rounded bg-white flex items-center justify-center ">
-              <Image
-                width={14}
-                height={12}
-                src="/assets/sections/team-members/icon-vimeo.svg"
-                alt="icon"
-              />
-            </div>
-          </a>
-        </div>
-      )}
-    </div>
-  );
-}
-function Team(prop) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const res = fetch(
-      process.env.NEXT_PUBLIC_REST_API_ENDPOINT + "/team-members"
-    );
-    setData(res.json());
-  }, []);
-  return (
-    <div data-aos="fade-up" className="team w-full h-auto mt-11 ">
-      {data ?? data.slice(0, prop.limit).map((profile) => (
-        <TeamProfile key={profile.id} {...profile} />
-      ))}
-    </div>
   );
 }
