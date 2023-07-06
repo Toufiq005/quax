e59b059a33aa5742555c9697f56e8b4ee144a6d5
Toufiq005 <125236484+Toufiq005@users.noreply.github.com>
2023-07-06 11:51:19 +0600

    bug fix

--- src/components/ui/Reveal/Reveal.jsx
+++ src/components/ui/Reveal/Reveal.jsx
@@ -4,13 +4,13 @@ import "./Reveal.css";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
 
-export default function Reveal({ children, className }) {
+export default function Reveal({ children, className, key }) {
   const ref = useRef(null);
   const inView = useInView(ref, { once: true });
 
   return (
     <>
-      <div ref={ref} className={className}>
+      <div ref={ref} key={key} className={className}>
         {inView ? children : null}
       </div>
     </>
